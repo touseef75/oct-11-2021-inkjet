b917970e857bbefd7833344c48f1636528e2ab20
touseef75 <imrantouseef776@gmail.com>
2021-10-14 01:08:50 -0700

    set

--- Assets/main.js
+++ Assets/main.js
@@ -2,14 +2,17 @@ let video = document.getElementById("video");
 let play = document.getElementById("play");
 let refresh = document.getElementById("Refresh");
 let audio = document.getElementById("audio");
+let img = document.querySelector("img");
 
 num = 0;
 play.addEventListener("click",()=>{
     if(num == 0){
+    img.src = "./Assets/images/pause.png";
     play.value = "pause";
     video.play();
      num = 1;
     }else if(num == 1){
+        img.src = "./Assets/images/play3.png";
         play.value = "play";
         video.pause();
         num = 0;
